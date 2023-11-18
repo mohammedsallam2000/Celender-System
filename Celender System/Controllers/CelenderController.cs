fc9fac75fe33adf260d0f4faa7e74bacff106f1e
mohammedsallam812@gmail.com <mohammedsallam812@gmail.com>
2023-11-19 01:36:52 +0300

    Sending Mail Celendar (Daily-Weekly-Monthly-Yearly) using Hangfire

--- Celender System/Controllers/CelenderController.cs
+++ Celender System/Controllers/CelenderController.cs
@@ -4,6 +4,8 @@ using System.Net;
 using Hangfire;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 
 namespace Celender_System.Controllers
 {
@@ -11,76 +13,92 @@ namespace Celender_System.Controllers
     [Route("[controller]")]
     public class CelenderController : ControllerBase
     {
-       
+        private readonly IConfiguration _configuration;
+
+        public CelenderController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpGet]
        // [Obsolete]
         public IActionResult Get()
         {
-            //BackgroundJob.Enqueue(() => SendMessage());
-
-            //Console.WriteLine(DateTime.Now);
-            //BackgroundJob.Schedule(() => Celender(), TimeSpan.FromMinutes(1));
-
-            //DateTime currentDate = DateTime.Now.Date;
-            //Console.WriteLine(currentDate.ToShortDateString());
-
-
             //Celender("Minutely");
             RecurringJob.AddOrUpdate(() => Celender("Minutely"), Cron.Minutely);
             //RecurringJob.AddOrUpdate(() => Celender("Daily"), Cron.Daily);
-            //RecurringJob.AddOrUpdate(() => Celender("Monthly"), Cron.Monthly);
-            //RecurringJob.AddOrUpdate(() => Celender("Yearly"), Cron.Yearly);
 
             return Ok("OK");
         }
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public DataTable ExecuteStoredProcedure(string storedProcedureName, string connectionStringName, SqlParameter[]? parameters)
+        {
+            string connectionString = _configuration.GetConnectionString(connectionStringName);
 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(storedProcedureName, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    // Add parameters to the command
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+                    connection.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        connection.Close();
+                        return dataTable;
+                    }
+                }
+            }
+        }
 
         [ApiExplorerSettings(IgnoreApi = true)]
         public void Celender(string SendTime)
         {
-            // string connectionString = "Data Source=192.168.7.9;Initial Catalog=Z2DataCore;Integrated Security=False;Persist Security Info=False;User ID=sobhy;Password=sobhy@1234;MultipleActiveResultSets=True;Connect Timeout=9999"
-            string connectionString = "Server=.; Database=CelenderDB; Integrated Security=True;TrustServerCertificate=True";
-            // Fetch data from the table (assuming you have a table named 'Users' with 'Email' column)
-            DataTable usersTable = new DataTable();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = "SELECT * FROM dbo.EmailCalenderSystem";
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    connection.Open();
-                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
-                    {
-                        adapter.Fill(usersTable);
-                    }
-                    connection.Close();
-                }
-            }
+
+            var dt = ExecuteStoredProcedure("sp_GetAllEmailCelender", "DefaultConnection",null);
+            //// string connectionString = "Data Source=192.168.7.9;Initial Catalog=Z2DataCore;Integrated Security=False;Persist Security Info=False;User ID=sobhy;Password=sobhy@1234;MultipleActiveResultSets=True;Connect Timeout=9999"
+            //string connectionString = "Server=.; Database=CelenderDB; Integrated Security=True;TrustServerCertificate=True";
+            //// Fetch data from the table (assuming you have a table named 'Users' with 'Email' column)
+            //DataTable usersTable = new DataTable();
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    string query = "SELECT * FROM dbo.EmailCalenderSystem";
+            //    using (SqlCommand command = new SqlCommand(query, connection))
+            //    {
+            //        connection.Open();
+            //        using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            //        {
+            //            adapter.Fill(usersTable);
+            //        }
+            //        connection.Close();
+            //    }
+            //}
 
 
             // Send email to each user
-
-            foreach (DataRow row in usersTable.Rows)
+            //DataTable dtt = new DataTable();
+            foreach (DataRow row in dt.Rows)
             {
                 string fromEmail = row["FromEmail"].ToString();
                 string toEmail = row["ToEmail"].ToString();
                 string FromPassword = row["FromPassword"].ToString();
                 string emailSubject = row["EmailSubject"].ToString();
                 string emailBody = row["EmailBody"].ToString();
-                string sendTime = row["SendTime"].ToString();
+                int CalenderID = (int)row["CalenderID"];
 
-                Console.WriteLine(fromEmail);
-                Console.WriteLine(toEmail);
-                Console.WriteLine(emailSubject);
-                Console.WriteLine(emailBody);
-                Console.WriteLine(sendTime);
-                SendEmail(fromEmail, FromPassword, toEmail, emailSubject, emailBody);
-
+                SendEmail(fromEmail, FromPassword, toEmail, emailSubject, emailBody, CalenderID);
 
             }
         }
-
-        static void SendEmail(string fromEmail, string fromPassword, string toEmail, string subject, string body)
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public void SendEmail(string fromEmail, string fromPassword, string toEmail, string subject, string body,int CalenderID)
         {
 
 
@@ -99,6 +117,12 @@ namespace Celender_System.Controllers
             {
                 smtpClient.Send(fromEmail, toEmail, subject, body);
                 Console.WriteLine($"Email sent successfully to {toEmail}");
+                SqlParameter[] parameters = new SqlParameter[]
+                {
+                    new SqlParameter("@CalenderID", CalenderID)
+                };
+                var dt =  ExecuteStoredProcedure("sp_EmailCelenderUpdateRunDate", "DefaultConnection", parameters);
+                Console.WriteLine(dt);
             }
             catch (Exception ex)
             {
